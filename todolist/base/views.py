b477ba0f1d80852a752cf05388efc0b0727ecbc9
Aayush <just.another.3d.object@gmail.com>
2022-07-17 02:41:43 +0530

    Changed test view (TaskList) from func view to CBV

--- todolist/base/views.py
+++ todolist/base/views.py
@@ -1,9 +1,7 @@
-import imp
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.views.generic import ListView
 # Create your views here.
 
 
-def taskList(request):
-    return HttpResponse("ToDo List")
+class TaskList(ListView):
+    model = Task
