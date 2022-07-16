9d5a0ea260d4c487c0d1ded7a752e8d450d69179
Aayush <just.another.3d.object@gmail.com>
2022-07-17 02:27:27 +0530

    Created attributes for Task Model

--- todolist/base/models.py
+++ todolist/base/models.py
@@ -6,8 +6,9 @@ from django.contrib.auth.models import User
 
 class Task(models.Model):
 
-    user =
-    title =
-    description =
-    complete =
-    create =
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=150, null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
+    complete = models.BooleanField(default=False)
+    create = models.DateTimeField(auto_now_add=True)
