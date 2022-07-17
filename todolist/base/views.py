5dd39886d350492ac7bd58b951302036e46ef0d8
Aayush <just.another.3d.object@gmail.com>
2022-07-17 16:58:56 +0530

    Added CBV for LoginView

--- todolist/base/views.py
+++ todolist/base/views.py
@@ -1,7 +1,17 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Task
 from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
 # Create your views here.
+
+
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
 
 
 class TaskList(ListView):
